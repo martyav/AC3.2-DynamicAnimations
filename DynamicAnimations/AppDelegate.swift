7ef2429b52532544bb3ff5f1ad259043becc1fdc
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-05-14 22:17:03 -0400

    testing...the remotes seem to be out of sync?

--- DynamicAnimations/AppDelegate.swift
+++ DynamicAnimations/AppDelegate.swift
@@ -49,26 +49,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    /*
-     class AppDelegate: UIResponder, UIApplicationDelegate {
-     
-     var window: UIWindow?
-     
-     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-     
-     self.window = UIWindow(frame: UIScreen.main.bounds)
-     let rootVC = EmptyDeckViewController()
-     let navController = UINavigationController(rootViewController: rootVC)
-     
-     self.window?.rootViewController = navController
-     self.window?.makeKeyAndVisible()
-     
-     
-     return true
-     }
-
- */
-    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
